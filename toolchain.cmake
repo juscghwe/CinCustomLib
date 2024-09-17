b7f90858ce2030ac62b809005dc385853c971907
JulianSchwe <62502225+juscghwe@users.noreply.github.com>
2024-09-17 12:50:31 +0200

    Applied changes from main

---
+++ toolchain.cmake
@@ -0,0 +1,5 @@
+# toolchain.cmake
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_C_COMPILER C:/msys64/ucrt64/bin/gcc.exe)
+set(CMAKE_CXX_COMPILER C:/msys64/ucrt64/bin/g++.exe)
+set(CMAKE_MAKE_PROGRAM C:/msys64/usr/bin/ninja.exe)
