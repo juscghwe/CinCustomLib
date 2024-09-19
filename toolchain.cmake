05f27953b50dc7ea0be5c7cb7395c2dd7a43888b
JulianSchwe <62502225+juscghwe@users.noreply.github.com>
2024-09-19 08:43:23 +0200

    Cmake setup

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,5 +1,12 @@
 # toolchain.cmake
+
+# target system
 set(CMAKE_SYSTEM_NAME Windows)
+
+# compilers
 set(CMAKE_C_COMPILER C:/msys64/ucrt64/bin/gcc.exe)
 set(CMAKE_CXX_COMPILER C:/msys64/ucrt64/bin/g++.exe)
-set(CMAKE_MAKE_PROGRAM C:/msys64/usr/bin/ninja.exe)
+
+# make program
+set(CMAKE_MAKE_PROGRAM C:/msys64/ucrt64/bin/ninja.exe)
+set(CMAKE_GENERATOR "Ninja")
